d02876838fe63eaf788383024c8e4bf5faccf7e3
Karol <K_wlodarczyk@interia.pl>
2023-01-03 17:15:36 +0100

    work in progress

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // API : http://www.omdbapi.com/?i=tt3896198&apikey=bf1ef21a
 // API 2 https://www.omdbapi.com/?i=tt3896198&apikey=bf1ef21a&s=fast
-const filmsListEl = document.querySelector('.film-list')
+// http://img.omdbapi.com/?apikey=bf1ef21a&
 
 async function main() {
     const films = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=bf1ef21a&s=fast") 
     const filmsData = await films.json();
-    filmsListEl.innerHTML = filmsData.map (films => filmsHTML(films)).join("");
+    const filmsListEl = document.querySelector(".film-list")
+    console.log(filmsData)
+    filmsListEl.innerHTML = filmsData.Search.map((films) => filmsHTML(films)).join("");
 };
 
 main();
@@ -13,7 +15,7 @@ main();
 function filmsHTML(films) {
     return `<div class="film-card">
     <div class="film-card__container">
-        <h3 class="title__film">${films.title}</h3>
+        <h3 class="film__title">${films.Title}</h3>
         <p><b class="font">Year:</b>${films.Year}</p>
         <p><b class="font">Type:</b>${films.Type}</p>
         <p><b class="font">Img:</b>${films.Poster}</p>
