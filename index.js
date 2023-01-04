d44fe5587dc2921422da442b20e2667a66c5f10e
Karol <K_wlodarczyk@interia.pl>
2023-01-04 22:41:41 +0100

    small progress

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ async function main() {
     const filmsListEl = document.querySelector(".film-list")
     console.log(filmsData)
     filmsListEl.innerHTML = filmsData.Search.map((films) => filmsHTML(films)).join("");
+    // let filmsslice = filmsData.slice(0,6)
+    // console.log(filmsslice)
 };
 
 main();
@@ -16,9 +18,41 @@ function filmsHTML(films) {
     return `<div class="film-card">
     <div class="film-card__container">
         <h3 class="film__title">${films.Title}</h3>
-        <p><b class="font">Year:</b>${films.Year}</p>
-        <p><b class="font">Type:</b>${films.Type}</p>
-        <p><b class="font">Img:</b>${films.Poster}</p>
+        <p><b class="font">Year:</b> ${films.Year}</p>
+        <p><b class="font">Type:</b> ${films.Type}</p>
+        <p><b class="font"></b><img src="${films.Poster}" class= "img__scale"></p>
     </div>
 </div>`
 }
+
+// async function renderFilms() {
+//     const films = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=bf1ef21a&s=fast") 
+//     const filmsData = await films.json();
+//     const filmsListEl = document.querySelector(".film-list")
+//     filmsListEl.innerHTML = filmsData.Search.map((films) => filmsHTML(films)).join("");
+
+//     const filmsWrapper = document.querySelector('.film__wrapper')
+//     filmsWrapper.innerHTML = `<div class="film">
+//      <div class="film-card">
+//      <div class="film-card__container">
+//      <h3 class="film__title">${films.Title}</h3>
+//         <p><b class="font">Year:</b> ${films.Year}</p>
+//         <p><b class="font">Type:</b> ${films.Type}</p>
+//         <p><b class="font"></b><img src="${films.Poster}" class= "img__scale"></p>
+//      </div>
+//      </div>
+//      </div>`
+// }
+
+// renderFilms()
+
+function searchFilm() {
+    const loadFilmWrapper = document.querySelector('.film__wrapper')
+    const hideImg = document.querySelector('.img')
+    loading.classList += " .film__wrapper--loaded"
+    setTimeout(() => {
+        console.log('itworked')
+    },1000)
+}
+
+searchFilm()
